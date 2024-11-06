efc17b5bf34b1fdd9afa9ff2a544dda512080dc5
Prince kumar <166175291+01krPrince@users.noreply.github.com>
2024-11-06 19:40:17 +0530

    Delete Repository directory

--- Repository/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sdf.age.Repository;
-
-import com.sdf.age.Model.Answer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AnswerRepository extends MongoRepository<Answer , String> {
-
-}
