a091933e1fed9075fad38a31d625367d5e4a3765
Prince kumar <166175291+01krPrince@users.noreply.github.com>
2024-11-16 19:35:55 +0530

    clean repository

--- Backend/Student-Discussion-Forum/src/main/java/com/sdf/age/Student/Discussion/Forum/Repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sdf.age.Student.Discussion.Forum.Repository;
-
-
-import com.sdf.age.Student.Discussion.Forum.Model.Answer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AnswerRepository extends MongoRepository<Answer, String> {
-
-}
